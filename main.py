bcb00713f1b94d7afcb1b030eafcba6c7ba107da
paexternal303 <paexternal303@oaisb.info>
2025-03-28 22:57:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-A =[1,78,46,4,34,10,50,2]
-N = len(A)
-def Algorithm(A,N):
-    #B <- Array[N]
-    B = A[N]
-    B=[0]*N
-    for i in range(1,N):
-        B[A[i]]+=1
-        i=1
-    #for i <-- 1 to N
-    for j in range(1,N):
-    #for k <-- to B[j]
-        for k in range(0,B[j]):
-            A[i]=j
-            i+=1
-    return
+from blog.models import BlogPost
+from blog.views import render_post
 
-Algorithm(A,N)
-print(A)
+if __name__ == "__main__":
+    post = BlogPost("Hello", "This is a post about AI and tagging systems.")
+    print(render_post(post))
