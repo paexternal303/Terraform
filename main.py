566e038931ffad21e4768e4f49e3742a496f8a59
paexternal303 <paexternal303@oaisb.info>
2025-03-28 22:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+A =[1,78,46,4,34,10,50,2]
+N = len(A)
+def Algorithm(A,N):
+    #B <- Array[N]
+    B = A[N]
+    B=[0]*N
+    for i in range(1,N):
+        B[A[i]]+=1
+        i=1
+    #for i <-- 1 to N
+    for j in range(1,N):
+    #for k <-- to B[j]
+        for k in range(0,B[j]):
+            A[i]=j
+            i+=1
+    return
 
+Algorithm(A,N)
+print(A)
